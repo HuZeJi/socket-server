1334b42d168760e06ee5f3e651a64133af6ad507
Hugo Jimenez <hugojim123@gmail.com>
2022-05-13 19:04:51 -0600

    First commit - rest server

---
+++ README.md
@@ -0,0 +1,20 @@
+# Socket-Server
+
+Rebuild node modules
+
+```
+npm install
+```
+
+Generate DIST
+
+```
+tsc -w
+```
+
+Up server
+
+```
+nodemon dist/
+node dist/
+```
---
+++ routes/router.ts
@@ -0,0 +1,28 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/messages", (req: Request, res: Response) => {
+	res.json({
+		ok: true,
+		message: "Todo bien",
+	});
+});
+
+router.post("/messages", (req: Request, res: Response) => {
+	const body = req.body.body;
+	const from = req.body.from;
+
+	res.json({ ok: true, body, from });
+});
+
+router.post("/messages/:id", (req: Request, res: Response) => {
+	const id = req.params.id;
+
+	const body = req.body.body;
+	const from = req.body.from;
+
+	res.json({ ok: true, body, from, id });
+});
+
+export default router;
